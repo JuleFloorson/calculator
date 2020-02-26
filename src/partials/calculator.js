b1295b5f3fe8764dae256fed46591d203b69ed5f
JuleFloorson <jubo93@web.de>
2020-02-26 14:33:06 +0100

    Add result and clear button

--- src/partials/calculator.js
+++ src/partials/calculator.js
@@ -1,1 +1,38 @@
-document.querySelector(".calculator__output").value = "42";
+"use strict";
+
+const calculatorOutput = document.querySelector(".calculator__output");
+const calculatorInput = document.querySelectorAll(".calculator__input");
+const calculatorResult = document.querySelector(".calculator__result");
+const calculatorClear = document.querySelector(".calculator__delete");
+
+function add(numberOne, numberTwo) {
+  return numberOne + numberTwo;
+}
+function subtract(numberOne, numberTwo) {
+  return numberOne - numberTwo;
+}
+function divide(numberOne, numberTwo) {
+  return numberOne / numberTwo;
+}
+function multiply(numberOne, numberTwo) {
+  return numberOne * numberTwo;
+}
+const numberOne = Number(calculatorInput[5].innerText);
+const numberTwo = Number(calculatorInput[6].innerText);
+
+//console.log() ist das gleich wie printf()
+//console.log(numberOne, numberTwo, "Hello");
+
+//calculatorOutput.value = add(numberOne, numberTwo);
+function handleClick() {
+  calculatorOutput.value = subtract(numberOne, numberTwo);
+
+  console.log("clicked");
+}
+calculatorResult.addEventListener("click", handleClick);
+
+function clear() {
+  calculatorOutput.value = "";
+}
+
+calculatorClear.addEventListener("click", clear);
